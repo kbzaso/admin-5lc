2c4859ab9b37b4982ca35696b4cad2788d9e55df
Alejandro Sáez <alejandro.sapa@gmail.com>
2023-12-14 23:37:44 -0300

    add anchor styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,8 +23,8 @@
 				<!-- row 1 -->
 				<tr class="hover">
 					<th>{product.id.substring(0, 6)}</th>
-					<a href={`/eventos/${product.id}`}>
-						<td>{product.name}</td>
+					<a class="hover:underline text-blue-400 hover:text-primary" href={`/eventos/${product.id}`}>
+						<td class="flex gap-2">{product.name} <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M384 224v184a40 40 0 0 1-40 40H104a40 40 0 0 1-40-40V168a40 40 0 0 1 40-40h167.48M336 64h112v112M224 288L440 72"/></svg></td>
 					</a>
 					<td>{product.stock}</td>
 					<!-- <td>{ticketsSold._sum.ticketAmount}</td> -->
